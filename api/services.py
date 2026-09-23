2b8861acc96ae4fc573851e440aa6737e0811cf4
sufyan72562 <sufyan72562@gmail.com>
2026-09-23 20:59:37 +0500

    ticket reservation with F expression

--- api/services.py
+++ api/services.py
@@ -1,6 +1,7 @@
 from api.models import Event, Ticket
 from rest_framework.exceptions import ValidationError
 from django.db import transaction
+from django.db.models import F
 
 class EventService:
     def get_event_by_id(self, event_id):
@@ -15,25 +16,25 @@ class EventService:
 
 class TicketService:
     def reserve_ticket(self, event_id, user):
-        try:
-            with transaction.atomic():
-
-                event = Event.objects.select_for_update().get(id=event_id)
-
-                if event.available_ticket_count <= 0:
-                    raise ValidationError("Event tickets are sold out!")
-                event.available_ticket_count -= 1
-                event.save(update_fields=["available_ticket_count"])
-
-                ticket = Ticket.objects.create(
-                    event=event,
-                    user=user,
-                    status="BOOKED"
+        with transaction.atomic():
+            if not Event.objects.filter(id=event_id).exists():
+                raise ValidationError(
+                    "Event does not exist."
                 )
-                return ticket
-       
-        except Event.DoesNotExist:
-            raise ValidationError("Event not found.")
+
+            updated = Event.objects.filter(id=event_id, 
+                                    available_ticket_count__gt=0).update(
+                                        available_ticket_count=F('available_ticket_count') - 1)
+
+            if updated == 0:
+                raise ValidationError("No available tickets for this event.")
+
+            ticket = Ticket.objects.create(
+                event_id=event_id,
+                user=user,
+                status="BOOKED"
+            )
+            return ticket
         
        
       
